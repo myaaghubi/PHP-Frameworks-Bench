2536a7722b7630f801eba4978bca4f51761a68df
Mohammad Yaaghubi <m.yaghobi.abc@gmail.com>
2023-05-13 11:31:32 +0330

    Dir `bin` deleted

--- bin/show_results_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/../libs/parse_results.php';
-require __DIR__ . '/../libs/build_table.php';
-
-$results = parse_results(__DIR__ . '/../output/results.hello_world.log');
-
-echo build_table($results);
