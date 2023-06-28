0559a91b3eac1ba2df3e6a07a76042d881ff3bd8
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2023-06-28 17:17:39 +0330

    A bug fix with the `bash show-table.sh`

--- libs/show_results_table.php
+++ libs/show_results_table.php
@@ -1,8 +1,8 @@
 <?php
 
-require __DIR__ . '/../libs/parse_results.php';
-require __DIR__ . '/../libs/build_table.php';
+require './libs/parse_results.php';
+require './libs/build_table.php';
 
-$results = parse_results(__DIR__ . '/../output/results.hello_world.log');
+$results = parse_results('./output/results.hello_world.log');
 
 echo build_table($results);
