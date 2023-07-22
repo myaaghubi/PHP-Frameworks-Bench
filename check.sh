9f374ef541fe308182d6a1d466a181c53b3dbec0
Joanhey <joanhey@kumbiaphp.com>
2023-07-23 00:13:48 +0200

    Change check.sh output

--- check.sh
+++ check.sh
@@ -3,6 +3,9 @@
 . ./benchmark.config
 . ./base/option_target.sh
 
+OK='\U2714'
+FAIL='\U274C'
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -10,17 +13,17 @@ NC='\033[0m' # No Color
 for fw in `echo $param_targets`
 do
     if [ -d "$fw" ]; then
-        echo -n "/------- $fw: checking... "
         . "$fw/_benchmark/hello_world.sh"
 
         url_output=$(curl -s "$url")
 
         # expected to get the Hello World! + libs/output_data.php
         if ! [[ "$url_output" =~ ^('Hello World!')(.*)(([0-9]*):(([0-9]+([.][0-9]*)?|[.][0-9]+)):([0-9]*))$ ]]; then
-            echo -e "${RED}error: \n$url${NC}"
+            echo -e "${RED}${FAIL} $fw ${NC}"
+            echo "$url"
             echo "$url_output"
         else
-            echo -e "${GREEN}done!${NC}"
+            echo -e "${GREEN}${OK} $fw ${NC}"
         fi
     fi
 done
