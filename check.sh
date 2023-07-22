17817129b1453b0bc08817df1ae5eb3ef37294b4
Joanhey <joanhey@kumbiaphp.com>
2023-07-22 23:25:50 +0200

    Add color to check.sh

--- check.sh
+++ check.sh
@@ -3,6 +3,10 @@
 . ./benchmark.config
 . ./base/option_target.sh
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 for fw in `echo $param_targets`
 do
     if [ -d "$fw" ]; then
@@ -13,10 +17,10 @@ do
 
         # expected to get the Hello World! + libs/output_data.php
         if ! [[ "$url_output" =~ ^('Hello World!')(.*)(([0-9]*):(([0-9]+([.][0-9]*)?|[.][0-9]+)):([0-9]*))$ ]]; then
-            echo -e "error: \n$url"
+            echo -e "${RED}error: \n$url${NC}"
             echo "$url_output"
         else
-            echo "done."
+            echo "${GREEN}done.${NC}"
         fi
     fi
 done
