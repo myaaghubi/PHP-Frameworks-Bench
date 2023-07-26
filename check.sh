e02e1e05c81653a75dd46e71611036e84802da5d
Joanhey <joanhey@kumbiaphp.com>
2023-07-26 13:03:58 +0200

    Add extra tab

--- check.sh
+++ check.sh
@@ -30,7 +30,7 @@ do
 
             FAIL=1
         else
-            echo -e "${GREEN}✔ $fw ${NC} \t ${#url_output} bytes"
+            echo -e "${GREEN}✔ $fw ${NC} \t\t ${#url_output} bytes"
         fi
     fi
 done
