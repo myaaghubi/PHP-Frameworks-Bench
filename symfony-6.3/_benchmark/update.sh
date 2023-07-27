78e2dfc63c072d8c1d3fc78d5d3bf1108c93ca0c
Joanhey <joanhey@kumbiaphp.com>
2023-07-27 13:39:53 +0200

    Fix Symfony 6.3 permissions

--- symfony-6.3/_benchmark/update.sh
+++ symfony-6.3/_benchmark/update.sh
@@ -7,4 +7,5 @@ yes|cp -r _benchmark/symfony/* ./
 # some enhancements
 composer dump-env prod
 composer install --no-dev --optimize-autoloader
-APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+APP_ENV=prod APP_DEBUG=0 bin/console cache:clear
+chmod -R o+w var
