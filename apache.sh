c198c7c53582c473e51c95c1b436a334ea857576
Joanhey <joanhey@kumbiaphp.com>
2023-07-29 12:22:02 +0200

    Check if docker is installed

--- apache.sh
+++ apache.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+if [ ! `which docker` ]; then
+    echo "docker not found."
+    exit 1;
+fi
+
 docker build --tag bench/apache -f .docker/apache.dockerfile .
 
 echo "Runing apache in http://127.0.0.1:8080"
