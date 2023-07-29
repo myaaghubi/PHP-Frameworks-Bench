43351a5c69535578729219d1ae7e6b5bfe867c8a
Joanhey <joanhey@kumbiaphp.com>
2023-07-29 14:23:55 +0200

    Add info to stop Apache

--- apache.sh
+++ apache.sh
@@ -10,5 +10,6 @@ PORT=${1:-8080}
 docker build --tag bench/apache -f .docker/apache.dockerfile .
 
 echo "Runing apache in http://127.0.0.1:$PORT"
+echo -e "Press Ctrl+C to stop. \n\n"
 
 docker run -it -p "$PORT":80 -v "$PWD":/var/www/html/PHP-Frameworks-Bench:rw bench/apache:latest
