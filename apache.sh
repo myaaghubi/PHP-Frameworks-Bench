0d04c926927b6b305d9300017f73fcea4254ad12
Joanhey <joanhey@kumbiaphp.com>
2023-07-29 12:39:52 +0200

    Add optional port input default port 8080

--- apache.sh
+++ apache.sh
@@ -5,8 +5,10 @@ if [ ! `which docker` ]; then
     exit 1;
 fi
 
+PORT=${1:-8080}
+
 docker build --tag bench/apache -f .docker/apache.dockerfile .
 
-echo "Runing apache in http://127.0.0.1:8080"
+echo "Runing apache in http://127.0.0.1:$PORT"
 
-docker run -it -p 8080:80 -v "$PWD":/var/www/html/PHP-Frameworks-Bench:rw bench/apache:latest
+docker run -it -p "$PORT":80 -v "$PWD":/var/www/html/PHP-Frameworks-Bench:rw bench/apache:latest
