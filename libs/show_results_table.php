ed3a6ccd01e6d98c5c7f7dbde30815d72607dc73
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2023-08-10 19:23:48 +0330

    Fix `show-table.sh` to pick the latest file

--- libs/show_results_table.php
+++ libs/show_results_table.php
@@ -3,6 +3,12 @@
 require './libs/parse_results.php';
 require './libs/build_table.php';
 
-$results = parse_results('./output/results.hello_world.log');
+$files = glob("./output/results.hello_world.*.log");
+
+rsort($files);
+
+echo @$files[0].PHP_EOL;
+
+$results = parse_results(@$files[0]);
 
 echo build_table($results);
