a217f5dcf754c555cf799f3e6f3293a39c9eacea
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2023-08-11 13:47:32 +0330

    Add more options

--- results.sh
+++ results.sh
@@ -5,6 +5,22 @@ if [ ! `which php` ]; then
     exit 1;
 fi
 
+function showHelp()
+{
+   cat << HEREDOC
+
+    Usage: bash results.sh [-l] [-t 1] [-d 2]
+
+    Optional Arguments:
+        -l, --list                      Show all results in order
+
+        -d [index], --delete [index]    Delete by index id (use --list)
+        -dall, --deleteall              Delete all results
+        -h, --help                      Show this help message and exit
+
+HEREDOC
+} 
+
 oldIFS="$IFS"
 paramsin="${@%/}"
 IFS='()'
@@ -20,6 +36,24 @@ results=(`ls ./output/`)
 for option in "${params[@]}"
 do
 	case "$option" in
+        -dall|--deleteall)
+            rm -rf ./output/*
+            ;;
+        -d*|--delete*)
+            delete=${option//--delete /}
+            delete=${option//-d /}
+            if [ ${#delete} -ge 0 ]; then
+                for i in "${!results[@]}"; do
+                    if [ $delete -eq $i ]; then
+                        echo "Delete ./output/${results[$i]}"
+                        rm -rf "./output/${results[$i]}"
+                        exit 1
+                    fi
+                done
+                echo "Index $delete not found!"
+            fi
+            exit 1
+            ;;
         -l|--list)
             echo "List of results: "
             for i in "${!results[@]}"; do
@@ -27,11 +61,14 @@ do
             done
             exit 1
             ;;
+        -h|--help)
+            showHelp;
+            exit 1
+            ;;
         ""|" ")
             ;;
         *)
             echo "\"${option}\" not available"
-            exit 1
             ;;
     esac
 done
