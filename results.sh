91a9ae7603696bdd4bb6413223d2224e51848c2c
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2023-08-11 13:23:09 +0330

    Add Ability to define options & Add `-l | --list`

--- results.sh
+++ results.sh
@@ -1,1 +1,39 @@
+#!/bin/sh
+
+if [ ! `which php` ]; then
+    echo "php not found."
+    exit 1;
+fi
+
+oldIFS="$IFS"
+paramsin="${@%/}"
+IFS='()'
+
+paramsin=(`php ./libs/trim.php "r" "${paramsin}"`)
+IFS=';'
+params=(`php ./libs/strreplace.php " -" ";-" " ${paramsin}"`)
+IFS=$oldIFS
+
+
+results=(`ls ./output/`)
+
+for option in "${params[@]}"
+do
+	case "$option" in
+        -l|--list)
+            echo "List of results: "
+            for i in "${!results[@]}"; do
+                echo "$i) ${results[$i]}"
+            done
+            exit 1
+            ;;
+        ""|" ")
+            ;;
+        *)
+            echo "\"${option}\" not available"
+            exit 1
+            ;;
+    esac
+done
+
 php ./libs/show_results_table.php
