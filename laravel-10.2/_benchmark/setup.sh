baaa19acda340e16c98dd751821bfe678f778ab9
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2023-11-29 14:34:33 +0330

    Update Laravel to version `10.2`

--- laravel-10.2/_benchmark/setup.sh
+++ laravel-10.2/_benchmark/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # create project
 rm -rf _benchmark/temp
-composer create-project --prefer-dist laravel/laravel:10.0.* ./_benchmark/temp
+composer create-project --prefer-dist laravel/laravel:10.2.* ./_benchmark/temp
 mv ./_benchmark/temp/{.,}* ./
 
 # have the route & controller
