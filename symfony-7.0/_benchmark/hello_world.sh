93996f8aec42fe923197bb1019b2108a3bf2f781
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2023-12-06 10:35:23 +0330

    Add `Symfony-7.0`

---
+++ symfony-7.0/_benchmark/hello_world.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+url="$base/$fw/public/index.php/hello/index"
---
+++ symfony-7.0/_benchmark/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# create project
+rm -rf _benchmark/temp
+composer create-project symfony/skeleton:7.0.* ./_benchmark/temp --ansi
+mv ./_benchmark/temp/{.,}* ./
+
+# have the route & controller
+yes|cp -r _benchmark/symfony/* ./
+
+# some enhancement
+composer dump-env prod --ansi
+APP_ENV=prod APP_DEBUG=0 bin/console cache:clear
+composer install --no-dev --optimize-autoloader --ansi
+chmod -R o+w var
