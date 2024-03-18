789018276d83e302ca053812a1dbece40f7585dd
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2024-03-18 18:10:13 +0330

    Dir check for `clear-cache.sh`

--- clear-cache.sh
+++ clear-cache.sh
@@ -9,6 +9,11 @@ fi
 . ./config
 . ./base/option_target.sh
 
+# Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 shopt -s extglob
 
 for fw in $param_targets
@@ -18,5 +23,9 @@ do
         cd "$fw"
         . "_benchmark/clear-cache.sh"
         cd ..
+    else
+        # check for dir
+        echo -e "${RED}Dir $fw/ doesn't exist!"
+        FAIL=1
     fi
 done
