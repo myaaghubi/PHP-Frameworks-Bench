a4b974d9b959972e35afe9dd4ac17e3d91fff4c4
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2024-03-18 17:59:35 +0330

    Dir check for `update.sh`

--- update.sh
+++ update.sh
@@ -7,12 +7,21 @@ fi
 . ./config
 . ./base/option_target.sh
 
+# Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 for fw in $param_targets
 do
+    echo "/------- $fw: updating -------/"
     if [ -d "$fw" ]; then
-        echo "/------- $fw: updating -------/"
         cd "$fw"
         . "_benchmark/update.sh"
         cd ..
+    else
+        # check for dir
+        echo -e "${RED}❌ Dir $fw ${NC} doesn't exist"
+        FAIL=1
     fi
 done
