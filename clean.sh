0a9ab7e7ac87540842f64e39eb5ae6ebf6ebc7a2
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2024-03-18 18:15:01 +0330

    Dir check for `clean.sh`

--- clean.sh
+++ clean.sh
@@ -3,6 +3,11 @@
 . ./config
 . ./base/option_target.sh
 
+# Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 shopt -s extglob
 
 for fw in $param_targets
@@ -12,5 +17,9 @@ do
         cd "$fw"
         . "_benchmark/clean.sh"
         cd ..
+    else
+        # check for dir
+        echo -e "${RED}Dir $fw/ doesn't exist!"
+        FAIL=1
     fi
 done
