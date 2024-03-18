54439683d0bd7d7c5b884cd570b7906d10c2f118
Mohammad Yaaghubi <m.yaaghubi.abc@gmail.com>
2024-03-18 18:17:17 +0330

    Dir check for `setup.sh`

--- setup.sh
+++ setup.sh
@@ -25,6 +25,11 @@ fi
 . ./config
 . ./base/option_target.sh
 
+# Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 for fw in $param_targets
 do
     if [ -d "$fw" ]; then
@@ -33,6 +38,10 @@ do
         cd "$fw"
         . "_benchmark/setup.sh"
         cd ..
+    else
+        # check for dir
+        echo -e "${RED}Dir $fw/ doesn't exist!"
+        FAIL=1
     fi
 done
 
