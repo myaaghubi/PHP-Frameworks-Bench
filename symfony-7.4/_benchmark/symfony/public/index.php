f5e03852eb2c8baae8241207d276a9023a524e44
Joan Miquel <joanhey@kumbiaphp.com>
2025-12-07 14:38:16 +0100

    Update index.php

--- symfony-7.4/_benchmark/symfony/public/index.php
+++ symfony-7.4/_benchmark/symfony/public/index.php
@@ -1,16 +1,29 @@
 <?php
-/*
-    PHP-Frameworks-Bench
-    this is a simple hello world controller to make benchmark
- */
-use App\Kernel;
 
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__.'/../vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-$kernel = new Kernel('prod', false);
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
